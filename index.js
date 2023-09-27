b2a5f2e0d0946e4ea3f352af921809b8a66a08d2
Klow420 <keiran-m420@hotmail.com>
2023-09-27 12:56:56 +0400

    ajout base de donnée

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 console.log("coucou toto");
+
+// import du packer mysql pour pouvoir l'utiliser ici
+import mysql from "mysql"
+const connection = mysql.createConnection({
+  host     : 'localhost',
+  port : '8889',
+  user     : 'root',
+  password : 'root',
+  database : 'rest',
+  socket : '/Applications/MAMP/tmp/mysql/mysql.sock'
+});
+ 
+connection.connect((err)=> {
+    if (err) throw err
+    console.log("base de donnée connecté");
+});
